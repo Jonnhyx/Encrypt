50e320e5efe7d8b8e5031d0cc2365cc44dd6652f
Jonatan <34280963+Jonnhyx@users.noreply.github.com>
2019-10-13 02:42:13 +0200

    Clase Encrypt
    Contine métodos para encriptar cadenas:
    
    MD5-SHA1-SHA384-SHA512
    
    BASE64: Encode - Decode

---
+++ ENCRYPT/ENCRYPT/Encrypt.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+
+namespace ENCRYPT
+{
+    class Encrypt
+    {
+        public static string MD5(string str){
+            MD5 md5 = MD5CryptoServiceProvider.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = md5.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+        public static string Base64Encode(string str){
+            byte[] byt = System.Text.Encoding.UTF8.GetBytes(str);
+            return Convert.ToBase64String(byt);
+        }
+        public static string Base64Decode(string str){
+            byte[] b = Convert.FromBase64String(str);
+            return System.Text.Encoding.UTF8.GetString(b);
+        }
+        public static string SHA1(string str){
+            SHA1 sha1 = SHA1Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha1.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+        public static string SHA256(string str){
+            SHA256 sha256 = SHA256Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha256.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+        public static string SHA384(string str){
+            SHA384 sha384 = SHA384Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha384.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+        public static string SHA512(string str){
+            SHA512 sha512 = SHA512Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = sha512.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+    }
+}
